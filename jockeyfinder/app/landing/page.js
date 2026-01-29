8a5342d945fc8825e9607ef18c003086c018060c
wilieshout-blip <wilieshout@gmail.com>
2026-01-29 20:40:01 +1300

    Add landing page

--- jockeyfinder/app/landing/page.js
+++ jockeyfinder/app/landing/page.js
@@ -1,1 +1,13 @@
-jockeyfinder/app/landing/page.js
+export default function LandingPage() {
+  return (
+    <main style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>JockeyFinder</h1>
+      <p>Connecting trainers and jockeys.</p>
+
+      <div style={{ marginTop: "2rem" }}>
+        <a href="/login">Log in</a>{" | "}
+        <a href="/signup">Sign up</a>
+      </div>
+    </main>
+  );
+}
